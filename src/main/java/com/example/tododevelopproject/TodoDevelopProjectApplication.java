194bf739b15c65e47bff17ef346c9c6b3fad523c
tmdcksdl <tmdcksdl99@naver.com>
2024-12-13 16:10:15 +0900

    init Commit

---
+++ src/main/java/com/example/tododevelopproject/TodoDevelopProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.tododevelopproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoDevelopProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoDevelopProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tododevelopproject/TodoDevelopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tododevelopproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoDevelopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
