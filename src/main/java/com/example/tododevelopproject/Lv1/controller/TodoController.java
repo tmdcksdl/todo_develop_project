8e6f2df35bb0c4a15b4a089444c60e1c77d41024
tmdcksdl <tmdcksdl99@naver.com>
2024-12-13 20:18:45 +0900

    :sparkles: feat: Add findAll method in TodoController

--- src/main/java/com/example/tododevelopproject/Lv1/controller/TodoController.java
+++ src/main/java/com/example/tododevelopproject/Lv1/controller/TodoController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/todos")
 @RequiredArgsConstructor
@@ -19,11 +21,21 @@ public class TodoController {
     // 생성자
 
     // 기능
+    // ::: 일정 생성 API
     @PostMapping
     public ResponseEntity<TodoResponseDto> saveTodo(@RequestBody TodoRequestDto requestDto) {
 
         TodoResponseDto todoResponseDto = todoService.saveTodo(requestDto.getUsername(), requestDto.getTitle(), requestDto.getContents());
 
         return new ResponseEntity<>(todoResponseDto, HttpStatus.CREATED);
+    }
+
+    // ::: 전체 일정 조회 API
+    @GetMapping
+    public ResponseEntity<List<TodoResponseDto>> findAll() {
+
+        List<TodoResponseDto> todoResponseDtoList = todoService.findAll();
+
+        return new ResponseEntity<>(todoResponseDtoList, HttpStatus.OK);
     }
 }
