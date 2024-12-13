4d4804480b9934ad358a254ac703613c71a661ea
tmdcksdl <tmdcksdl99@naver.com>
2024-12-13 19:47:22 +0900

    :sparkles: feat: Add TodoRequestDto

---
+++ src/main/java/com/example/tododevelopproject/Lv1/dto/TodoRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.tododevelopproject.Lv1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class TodoRequestDto {
+
+    // 속성
+    private final String username;
+
+    private final String title;
+
+    private final String contents;
+
+    // 생성자
+    public TodoRequestDto(String username, String title, String contents) {
+        this.username = username;
+        this.title = title;
+        this.contents = contents;
+    }
+
+
+    // 기능
+}
