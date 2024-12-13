4ccc9c4647bd5346d03aa739719733481cd4d089
tmdcksdl <tmdcksdl99@naver.com>
2024-12-13 20:38:53 +0900

    :sparkles: feat: Add findByIdOrElseThrow method in TodoRepository

--- src/main/java/com/example/tododevelopproject/Lv1/repository/TodoRepository.java
+++ src/main/java/com/example/tododevelopproject/Lv1/repository/TodoRepository.java
@@ -2,6 +2,12 @@ package com.example.tododevelopproject.Lv1.repository;
 
 import com.example.tododevelopproject.Lv1.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    default Todo findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id));
+    }
 }
