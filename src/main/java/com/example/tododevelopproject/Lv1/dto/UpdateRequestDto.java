df430d7e4bfd468941d76378f7da672a7d0c943f
tmdcksdl <tmdcksdl99@naver.com>
2024-12-14 15:08:55 +0900

    :sparkles: feat: Add UpdateRequestDto

---
+++ src/main/java/com/example/tododevelopproject/Lv1/dto/UpdateRequestDto.java
@@ -0,0 +1,21 @@
+package com.example.tododevelopproject.Lv1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UpdateRequestDto {
+
+    // 속성
+    private final String title;
+
+    private final String contents;
+
+    // 생성자
+    public UpdateRequestDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+
+
+    // 기능
+}
