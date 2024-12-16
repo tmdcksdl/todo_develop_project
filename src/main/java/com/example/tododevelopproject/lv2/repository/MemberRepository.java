33b865c99e612333877424c53b721f9aec22e21f
tmdcksdl <tmdcksdl99@naver.com>
2024-12-16 10:12:25 +0900

    :sparkles: feat: Add findByIdOrElseThrow method in MemberRepository

--- src/main/java/com/example/tododevelopproject/lv2/repository/MemberRepository.java
+++ src/main/java/com/example/tododevelopproject/lv2/repository/MemberRepository.java
@@ -7,5 +7,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-
+    default Member findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id));
+    }
 }
