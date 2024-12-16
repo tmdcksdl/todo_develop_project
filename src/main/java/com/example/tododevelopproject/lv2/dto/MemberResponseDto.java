1648a8dc0a1745dd83dcf906a79ae2fb220fa8f7
tmdcksdl <tmdcksdl99@naver.com>
2024-12-16 09:57:18 +0900

    :sparkles: feat: Add MemberResponseDto

---
+++ src/main/java/com/example/tododevelopproject/lv2/dto/MemberResponseDto.java
@@ -0,0 +1,25 @@
+package com.example.tododevelopproject.lv2.dto;
+
+import com.example.tododevelopproject.lv2.entity.Member;
+import lombok.Getter;
+
+@Getter
+public class MemberResponseDto {
+
+    // 속성
+    private final Long id;
+
+    private final String username;
+
+    private final String email;
+
+    // 생성자
+    public MemberResponseDto(Long id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+
+    // 기능
+
+}
