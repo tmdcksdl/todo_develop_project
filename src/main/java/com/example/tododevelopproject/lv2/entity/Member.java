031c4bb3a934db470b1a0b55a98216335888cb87
tmdcksdl <tmdcksdl99@naver.com>
2024-12-16 09:56:33 +0900

    :sparkles: feat: Add Member entity

---
+++ src/main/java/com/example/tododevelopproject/lv2/entity/Member.java
@@ -0,0 +1,33 @@
+package com.example.tododevelopproject.lv2.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "member")
+@NoArgsConstructor
+public class Member extends BaseMemberEntity{
+
+    // 속성
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Email
+    private String email;
+
+    // 생성자
+    public Member(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+
+    // 기능
+}
