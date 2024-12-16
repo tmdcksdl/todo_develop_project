771dfea1bfcba15cfa27c9a33dd607e1ce1db6a9
tmdcksdl <tmdcksdl99@naver.com>
2024-12-16 11:40:10 +0900

    :fire: refactor: Remove username from TodoResponseDto

--- src/main/java/com/example/tododevelopproject/lv1/dto/TodoResponseDto.java
+++ src/main/java/com/example/tododevelopproject/lv1/dto/TodoResponseDto.java
@@ -9,17 +9,14 @@ public class TodoResponseDto {
     // 속성
     private final Long id;
 
-    private final String username;
-
     private final String title;
 
     private final String contents;
 
     // 생성자
 
-    public TodoResponseDto(Long id, String username, String title, String contents) {
+    public TodoResponseDto(Long id, String title, String contents) {
         this.id = id;
-        this.username = username;
         this.title = title;
         this.contents = contents;
     }
@@ -27,6 +24,6 @@ public class TodoResponseDto {
 
     // 기능
     public static TodoResponseDto toDo(Todo todo) {
-        return new TodoResponseDto(todo.getId(), todo.getUsername(), todo.getTitle(), todo.getContents());
+        return new TodoResponseDto(todo.getId(), todo.getTitle(), todo.getContents());
     }
 }
